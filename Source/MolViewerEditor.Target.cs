cc01769df33313f62ab6dd5d9e3d791f8f0ec90b
Will Masek <willmasek@gmail.com>
2024-07-23 19:58:02 +0100

    Updated for 5.4

--- Source/MolViewerEditor.Target.cs
+++ Source/MolViewerEditor.Target.cs
@@ -7,8 +7,11 @@ public class MolViewerEditorTarget : TargetRules
 {
 	public MolViewerEditorTarget( TargetInfo Target) : base(Target)
 	{
-		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "MolViewer" } );
-	}
+        Type = TargetType.Editor;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+
+        // Override build environment instead of setting it to unique
+        bOverrideBuildEnvironment = true;
+    }
 }
